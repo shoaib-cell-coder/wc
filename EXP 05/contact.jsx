44eb3042bf780611c3e8cc64bf4ff7a9492304e7
shoaib-cell-coder <23ds63@aiktc.ac.in>
2025-10-26 13:13:22 +0530

    Create contact.jsx

---
+++ EXP 05/contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Contact Us</h1>
+      <p>Email: bookstore@example.com</p>
+      <p>Phone: +91 9876543210</p>
+    </div>
+  );
+}
+
+export default Contact;
