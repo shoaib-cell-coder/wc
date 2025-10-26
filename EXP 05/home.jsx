6ba198c7486782d61cc0672f4fb957c67a312b2c
shoaib-cell-coder <23ds63@aiktc.ac.in>
2025-10-26 13:13:53 +0530

    Create home.jsx

---
+++ EXP 05/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Welcome to My Book Store 📚</h1>
+      <p>Find your favorite books and add them to your collection!</p>
+    </div>
+  );
+}
+export default Home;
